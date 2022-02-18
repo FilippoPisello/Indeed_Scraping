057f19b9f4cc3a2c1428f8f17be13b5e3234f757
Filippo <filippo.pisello@live.it>
2022-02-18 16:12:17 +0100

    First commit

---
+++ src/job_post.py
@@ -0,0 +1,79 @@
+from dataclasses import dataclass
+
+from bs4 import BeautifulSoup, element
+
+
+@dataclass
+class JobPost:
+    job_id: str
+    title: str
+    company: str
+    salary: str
+    rating_value: float
+    rating_count: int
+    contract_type: list[str]
+    description: str
+
+    @classmethod
+    def from_job_soup(cls, job_soup: BeautifulSoup, job_id: str):
+        title = cls.text_if_not_none(
+            job_soup.find("h1", {"class": "jobsearch-JobInfoHeader-title"})
+        )
+        company = cls.text_if_not_none(
+            job_soup.find("div", {"class": "jobsearch-CompanyReview--heading"})
+        )
+        salary = cls.text_if_not_none(
+            job_soup.find("span", {"class": "attribute_snippet"})
+        )
+        rating_value = cls.text_if_not_none(
+            job_soup.find("meta", {"itemprop": "ratingValue"}),
+            as_content=True,
+            new_type=float,
+        )
+
+        rating_count = cls.text_if_not_none(
+            job_soup.find("meta", {"itemprop": "ratingCount"}),
+            as_content=True,
+            new_type=int,
+        )
+
+        contract_type = cls.text_if_not_none(
+            job_soup.find("span", {"class": "jobsearch-JobMetadataHeader-item"})
+        )
+        if contract_type is not None:
+            contract_type = contract_type.replace(" -  ", "").split(", ")
+            contract_type = [c for c in contract_type if len(c) > 3]
+
+        description = cls.text_if_not_none(
+            job_soup.find("div", {"id": "jobDescriptionText"})
+        )
+        return cls(
+            job_id=job_id,
+            title=title,
+            company=company,
+            salary=salary,
+            rating_value=rating_value,
+            rating_count=rating_count,
+            contract_type=contract_type,
+            description=description,
+        )
+
+    @staticmethod
+    def text_if_not_none(
+        soup_search: element.Tag | None, as_content: bool = False, new_type=None
+    ):
+        if soup_search is not None:
+
+            if as_content:
+                output = soup_search["content"]
+            else:
+                output = soup_search.text
+
+            if new_type is not None:
+                return new_type(output)
+            return output
+
+        return None
+
+
+def jobs_to_json(job_list:list[jobs])
---
+++ src/scraping.py
@@ -0,0 +1,61 @@
+import json
+
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+from .job_post import JobPost
+
+
+def main(search: str | None = None, numb_pages: int = 1):
+    if search is None:
+        search = input("Please provide the search terms:\n")
+
+    single_url = search_to_url(search)
+    pages_urls = url_to_pages(single_url, numb_pages)
+
+    i = 0
+    job_ids = set()
+    for url in pages_urls:
+        soup = url_to_content(url)
+        job_ids = job_ids.union(set(get_job_ids_from_soup(soup)))
+
+    jobs_dict = {}
+    for index, job_id in enumerate(tqdm(job_ids)):
+        job_url = job_id_to_url(job_id)
+        job_page = requests.get(job_url)
+        job_soup = BeautifulSoup(job_page.content, "html.parser")
+        jobs_dict[job_id] = JobPost.from_job_soup(job_soup=job_soup, job_id=job_id)
+
+        if (index % 20 == 0) or ((index + 1) == len(jobs_dict)):
+            json_data = {key: jobs_dict[key].__dict__ for key in jobs_dict}
+            with open(f"saved_jobs_{search}_.json", "w", encoding="utf-8") as f:
+                json.dump(json_data, f, ensure_ascii=False, indent=4)
+
+    return jobs_dict
+
+
+def search_to_url(search: str) -> str:
+    """Transform user search terms into ready to use URL"""
+    search = search.lower().replace(" ", "%20")
+    return f"https://uk.indeed.com/jobs?q={search}&l=United%20Kingdom"
+
+
+def url_to_pages(url: str, number_pages: int) -> list[str]:
+    return [url + f"&start={n * 10}" for n in range(0, number_pages)]
+
+
+def url_to_content(url: str) -> requests.Response:
+    page = requests.get(url)
+    return BeautifulSoup(page.content, "html.parser")
+
+
+def get_job_ids_from_soup(soup) -> list[str]:
+    mosaic_zone = soup.find("div", {"id": "mosaic-zone-jobcards"})
+    mosaic_zone = mosaic_zone.find("div", {"id": "mosaic-provider-jobcards"})
+    a_results = mosaic_zone.find_all("a", href=True)
+    return [a.get("data-jk") for a in a_results if a.get("data-jk") is not None]
+
+
+def job_id_to_url(job_id: str) -> str:
+    return f"https://uk.indeed.com/viewjob?jk={job_id}"
